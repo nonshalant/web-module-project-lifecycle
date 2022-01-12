674bc570ccbeb331e789180507e33d51c2973958
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2022-01-11 19:35:30 -0500

    just finished building the ui for the project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
+    return(
+    <div>
+      <h1> GITHUB INFO</h1>
+      <form>
+        <input type='text' placeholder='Github Handle'></input>
+        <button>Search</button>
+      </form>
+      <img src="https://avatars.githubusercontent.com/u/63321977?v=4"/>
     </div>);
   }
 }
